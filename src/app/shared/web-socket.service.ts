7f953b9ae90c909f29581daae2d652cb7024ee51
Skyliss <ricardo.hdz.sm@hotmail.com>
2020-09-15 00:54:41 -0500

    Socket Service

---
+++ src/app/shared/web-socket.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebSocketService {
+  socket: any;
+  readonly url: string = 'ws://localhost:4000';
+
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+  listen(eventName: string): Observable<any> {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any): void {
+    this.socket.emit(eventName, data);
+  }
+}
